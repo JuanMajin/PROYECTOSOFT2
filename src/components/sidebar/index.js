ea7e97a585bc6655fe5d9d9951d60e8178aab68b
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2023-04-26 23:20:32 -0500

    Integrando spotify a la app de react

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import "./sidebar.css"
 import SidebarButton from './sidebarButton'
 import { MdDashboard, MdFavorite } from "react-icons/md"
@@ -6,12 +6,21 @@ import { FaGripfire, FaPlay } from "react-icons/fa"
 import { FaSignOutAlt } from "react-icons/fa"
 import { IoLibrary } from "react-icons/io5"
 import { MdSpaceDashboard } from "react-icons/md"
-
+import apiClient from "../../spotify";
 
 export default function Sidebar() {
+  const [image, setImage] = useState(
+    "https://c8.alamy.com/zoomses/9/9c30002a90914b58b785a537a39421ba/2c80ydc.jpg"
+  );
+  useEffect(() => {
+    apiClient.get("me").then((response) => {
+      setImage(response.data.images[0].url);
+    });
+  }, []);
+
   return (
     <div className="sidebar-container">
-      <img src="https://c8.alamy.com/zoomses/9/9c30002a90914b58b785a537a39421ba/2c80ydc.jpg" className="profile-img" alt="profile" />
+      <img src={image} className="profile-img" alt="profile" />
 
 
       <div>
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes,  } from 'react-router-dom'
 import Library from '../library/index'
 import Feed from '../feed/index'
@@ -7,22 +7,40 @@ import Player from '../player/index'
 import Favorites from '../favorites'
 import "./home.css";
 import Sidebar from '../../components/sidebar'
+import { setClientToken } from "../../spotify";
+import Login from '../auth/login'
 
 export default function Home() {
-  return (
-    <Router>
+  const [token, setToken] = useState("");
 
-    <div className="main-body">
-  
-    <Sidebar />
-    <Routes>
-        <Route path="/library" element= {<Library />} />
-        <Route path="/feed" element= {<Feed />} />
-        <Route path="/trending" element= {<Trending />} />
-        <Route path="/player" element= {<Player />} />
-        <Route path="/favorites" element= {<Favorites />} />
-    </Routes>
-    </div>
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
+  }, []);
+  return !token ? (
+    <Login />
+  ) : (
+    <Router>
+      <div className="main-body"> 
+        <Sidebar />
+        <Routes>
+          <Route path="/library" element= {<Library />} />
+          <Route path="/feed" element= {<Feed />} />
+          <Route path="/trending" element= {<Trending />} />
+          <Route path="/player" element= {<Player />} />
+          <Route path="/favorites" element= {<Favorites />} />
+        </Routes> 
+      </div>
     </Router>
-  )
+  );
 }
