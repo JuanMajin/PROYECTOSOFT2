b13406484f2905b9169f502cc69500caed518e6b
Alejand2r <alejorm44@gmail.com>
2023-05-03 10:38:38 -0500

    Uploading unregistered changes

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "d1fea24c7fef4a1b97c08f06012d4456";
-const redirectUri = "http://localhost:3000";
+const redirectUri = "http://localhost:3000/";
 const scopes = ["user-library-read", "playlist-read-private"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
