d0943571c694fe708a8a11715ffb12899af9a588
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2023-06-15 14:32:58 -0500

    Adjuntando a global.test

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,6 +1,12 @@
 import renderer from 'react-test-renderer';
 import Link from './prueba';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from '../components/audioPlayer/controls';
+import axios from 'axios';
+import { setClientToken } from '../spotify';
 
+jest.mock('axios');
 
 it('changes the class when hovered', () => {
   const component = renderer.create(
@@ -43,5 +49,72 @@ describe('ProductHeader', () => {
   })
 })
 
+describe('Controls', () => {
+  it('should call handlePrev when previous button is clicked', () => {
+    const handlePrevMock = jest.fn();
+    const { getByTestId } = render(
+      <Controls handlePrev={handlePrevMock} />
+    );
+
+    const prevButton = getByTestId('prev-button');
+    fireEvent.click(prevButton);
+
+    expect(handlePrevMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call handleNext when next button is clicked', () => {
+    const handleNextMock = jest.fn();
+    const { getByTestId } = render(
+      <Controls handleNext={handleNextMock} />
+    );
+
+    const nextButton = getByTestId('next-button');
+    fireEvent.click(nextButton);
+
+    expect(handleNextMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should toggle isPlaying state when play/pause button is clicked', () => {
+    const setIsPlayingMock = jest.fn();
+    const { getByTestId } = render(
+      <Controls isPlaying={false} setIsPlaying={setIsPlayingMock} />
+    );
+
+    const playPauseButton = getByTestId('play-pause-button');
+    fireEvent.click(playPauseButton);
+
+    expect(setIsPlayingMock).toHaveBeenCalledTimes(1);
+    expect(setIsPlayingMock).toHaveBeenCalledWith(true);
+  });
+});
+
+describe('apiClient', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should set client token in the request headers', async () => {
+    const token = 'your-access-token';
+
+    setClientToken(token);
+
+    // Verificar que la función use de interceptors.request se haya llamado
+    expect(axios.interceptors.request.use).toHaveBeenCalled();
+
+    // Obtener el callback pasado a la función use de interceptors.request
+    const callback = axios.interceptors.request.use.mock.calls[0][0];
+
+    // Crear una configuración de solicitud de prueba
+    const config = {
+      headers: {},
+    };
+
+    // Llamar al callback de la interceptación de solicitud con la configuración de prueba
+    const updatedConfig = await callback(config);
+
+    // Verificar que el token se haya establecido correctamente en la configuración
+    expect(updatedConfig.headers.Authorization).toBe(`Bearer ${token}`);
+  });
+})
 
 
